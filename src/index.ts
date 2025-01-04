6dca4a5604f793f9b640437a3d35f8ac76c66fb6
Jeremy Zabala <jeremy.zbala@gmail.com>
2025-01-04 16:20:17 -0600

    factory

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,99 @@
 import { Hono } from "hono";
+import { cache } from "hono/cache";
 import { serveStatic } from "hono/cloudflare-workers";
+import { cors } from "hono/cors";
 import { parse } from "node-html-parser";
 
 const CAR_LOGOS = "https://www.carlogos.org";
 
 const app = new Hono();
 
+app.use(
+  cors({
+    origin: "*",
+    allowMethods: ["GET"],
+  })
+);
+
 app.get("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
 app.get("/", (c) => c.text(`Overol CDN \ndocs: https://developers.overol.mx`));
 
-app.get("/cars/:brand/:model/:year", async (c) => {
-  const { brand, model, year } = c.req.param();
+app.get(
+  "/cars/:brand/:model/:year",
+  cache({
+    cacheName: "car-images",
+    cacheControl: "public, max-age=31536000, immutable",
+  }),
+  async (c) => {
+    const { brand, model, year } = c.req.param();
 
-  const args = new URLSearchParams({
-    make: brand,
-    modelYear: year,
-    modelFamily: model,
-    zoomType: "fullscreen",
-    customer: "hrjavascript-mastery",
-  });
+    const args = new URLSearchParams({
+      make: brand,
+      modelYear: year,
+      modelFamily: model,
+      zoomType: "fullscreen",
+      customer: "hrjavascript-mastery",
+    });
 
-  try {
-    const result = await fetch(
-      "https://cdn.imagin.studio/getimage?" + args.toString()
-    );
+    // c.header("Content-Type", "image/webp");
 
-    if (result.status !== 200) throw new Error("Error");
+    try {
+      const result = await fetch(
+        "https://cdn.imagin.studio/getimage?" + args.toString()
+      );
 
-    return new Response(result.body);
-  } catch (error) {
-    return c.json({ error: "Error" }, 500);
+      if (result.status !== 200) throw new Error("Error");
+
+      return c.body((await result.blob()) as any, 200, {
+        "Content-Type": "image/webp",
+      });
+    } catch (error) {
+      return c.json({ error: "Error" }, 500);
+    }
   }
-});
+);
 
-app.get("/brands/:brand", async (c) => {
-  const brand = c.req.param("brand");
+app.get(
+  "/brands/:brand",
+  cache({
+    cacheName: "car-logos",
+    cacheControl: "public, max-age=31536000, immutable",
+  }),
+  async (c) => {
+    const brand = c.req.param("brand");
 
-  const result = await fetch(`${CAR_LOGOS}/car-brands/${brand}-logo.html`);
+    const result = await fetch(`${CAR_LOGOS}/car-brands/${brand}-logo.html`);
 
-  const html = await result.text();
-  const root = parse(html);
+    const html = await result.text();
+    const root = parse(html);
 
-  const logo = (
-    root.querySelector("div.present a img") ||
-    root.querySelector("div.content p a img") ||
-    root.querySelector("div.current img")
-  )?.getAttribute("src") as string;
+    const logo = (
+      root.querySelector("div.present a img") ||
+      root.querySelector("div.content p a img") ||
+      root.querySelector("div.current img")
+    )?.getAttribute("src") as string;
 
-  const isRelative = logo.startsWith("/");
+    const isRelative = logo.startsWith("/");
 
-  const logoUrl = isRelative ? CAR_LOGOS + logo : logo;
+    const logoUrl = isRelative ? CAR_LOGOS + logo : logo;
 
-  const image = await fetch(logoUrl, {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-    },
-  });
+    const image = await fetch(logoUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      },
+    });
 
-  if (image.status !== 200) {
-    return c.json({ error: "Not found" }, 404);
+    if (image.status !== 200) return c.json({ error: "Not found" }, 404);
+
+    const mimeType = logo.split(".").pop();
+
+    // @ts-ignore
+    return c.body(await image.blob(), 200, {
+      "Content-Type": `image/${mimeType}`,
+    });
   }
-
-  const mimeType = logo.split(".").pop();
-
-  // @ts-ignore
-  return c.body(await image.blob(), 200, {
-    "Content-Type": `image/${mimeType}`,
-  });
-});
+);
 
 export default app;
